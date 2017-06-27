1d7a77d4ab5719e0353111985cfd2fbdb5358ceb
InnovateBham_10 <InnovateBham_10@Admins-iMac.local>
2017-06-27 16:25:04 -0500

    first commit

--- script.js
+++ script.js
@@ -1,34 +1,47 @@
 
+
 var sentences = ['ten ate neite ate nee enet ite ate inet ent eate',
                 'Too ato too nOt enot one totA not anot tOO aNot', 
                 'oat itain oat tain nate eate tea anne inant nean', 
                 'itant eate anot eat nato inate eat anot tain eat', 
                 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
 
-$(document).ready(function() {
+                $("#sentence").append(sentences[0]);
+var letters = 0
+var sentenceCounter = 0
+var currentSentence = sentences[sentenceCounter]
+  
+  $(document).ready(function() {
     $("#keyboard-upper-container").hide();
 
-    $(document).on('keydown', function(e) {
+  $(document).on('keydown', function(e) {
         handleHighlight(e.which);
-        console.log(e.which);
-        console.log(e.keyCode);
+         // console.log(e.which);
+        // console.log(e.keyCode);
 
-        if (e.keyCode === 16) {
+    if(e.which >=65 && e.which <=90) {
+        handleHighlight(e.which+32);
+    }
+
+    if (e.keyCode === 16) {
             $("#keyboard-upper-container").show();
             $("#keyboard-lower-container").hide();
         }
+        showText()
     });
 
-    $(document).on('keyup', function(e) {
+  $(document).on('keyup', function(e) {
         handleHighlight(e.which, true);
-
-        if (e.keyCode === 16) {
+    
+    if(e.which >=65 && e.which <=90) {
+         handleHighlight(e.which+32, true);
+    }
+    if (e.keyCode === 16) {
             $("#keyboard-upper-container").hide();
             $("#keyboard-lower-container").show();
         }
     });
-
-    $("#sentence").append(sentences[0]);
+    
 });
 
 function handleHighlight(code, defaultColor) {
@@ -45,9 +58,26 @@ function handleHighlight(code, defaultColor) {
         console.log('turning pink');
         $('#' + code).css('background-color' , 'pink');
     }
-}
+} 
 
 
+
+
+
+
+
+  var showText = function () {   
+  if (letters < currentSentence.length) {
+$('#target-letter').empty();
+    $('#target-letter').append(currentSentence[letters++]);
+  }
+  }
+    
+//     $(function () {
+
+//       showText("#target-letter");   
+
+// });  
 
 
 
